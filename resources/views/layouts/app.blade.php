7d364441f0bc548ec4f9cee4e268b472bb213aff
Pepe artacho org <sepepamartin@gmail.com>
2026-02-25 21:23:03 +0200

    refactor: comentar debug.footer temporalmente
    - Comentado @include('debug.footer') para ocultar panel de debug
    - Mantenido código comentado para uso futuro si se necesita
    - Footer de soporte flotante sigue activo
    - Layout más limpio para producción

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,7 +67,7 @@
                 {{ $slot }}
             </main>
             <div>
-                @include('debug.footer')
+                <!-- @include('debug.footer') -->
             </div>
             
             <!-- Footer con botón de soporte flotante -->
