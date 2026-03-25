f469ef2a7229e7adf148ff577ee8d199a825355d
Pepe artacho org <sepepamartin@gmail.com>
2026-03-25 19:56:11 +0200

    Add forgot password link to welcome page navigation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -58,8 +58,7 @@
                     @else
                         <a
                             href="{{ route('login') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
-                        >
+                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
                             {{ __('site.login') }}
                         </a>
 
@@ -70,6 +69,14 @@
                                     {{ __('site.register') }}
                             </a>
                         @endif -->
+
+                        {{-- Enlace de recordar contraseña --}}
+                        @if(Route::has('password.request'))
+                        <a href="{{ route('password.request') }}" 
+                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                            <span>{{ __('Has oblidat la teva contrasenya?') }}</span>
+                        </a>
+                        @endif
                     @endauth
                 </nav>
             @endif
